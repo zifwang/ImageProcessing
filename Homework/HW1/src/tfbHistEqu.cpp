619e86adb83b94004fd38e77aa90de22b5a2b97f
Zifan Wang <zifwang@Zifans-MacBook-Air.local>
2019-01-15 11:44:40 -0800

    Add Image Read In Method to tfgHistEqu.cpp

---
+++ Homework/HW1/src/tfbHistEqu.cpp
@@ -0,0 +1,63 @@
+// Problem 1. C. Transfer Function Based Histogram Equalization
+// Author: Zifan Wang
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int main(int argc, char *argv[]){
+    // Define variables
+    FILE *file;                                  // Read in file.
+    int BytesPerPixel = 3;                       // image is in gray scale, set this para. to 1. If RGB set to 3.
+    int ImageHeight = 300, ImageWidth = 390;     // Image size
+
+    // Check for proper syntax
+	if (argc < 3){
+		cout << "Syntax Error - Incorrect Parameter Usage:" << endl;
+		cout << "program_name input_image.raw output_image.raw [BytesPerPixel = 1] [ImageHeight = 256] [ImageWidth = 256]" << endl;
+		return 0;
+	}
+	
+	// Check if image is grayscale or color
+	if (argc < 4){
+		BytesPerPixel = 1; // default is grey image
+	} 
+	else {
+		BytesPerPixel = atoi(argv[3]);
+		// Check if size is specified
+		if (argc >= 5){
+			ImageHeight = atoi(argv[4]);
+            ImageWidth = atoi(argv[5]);
+		}
+	}
+
+    // Todo: Read raw image into program and store it in array
+    unsigned char imageData[ImageHeight][ImageWidth][BytesPerPixel];   // Image array to store .raw file
+    if(file = fopen(argv[1],"rb")){
+        fread(imageData,sizeof(unsigned char), ImageHeight*ImageWidth*BytesPerPixel, file);
+    }else{
+        cout << "Error opening file" << endl;
+        exit(1);
+    }
+
+
+
+
+
+
+
+
+
+
+    // Todo: Store image
+    if(!(file = fopen(argv[2],"wb"))){
+        cout << "Cannot open file: " << endl;
+        exit(1);
+    }
+    fwrite(imageData, sizeof(unsigned char), (ImageHeight)*(ImageWidth)*3, file);
+    fclose(file);
+    
+    return 0;
+}    
