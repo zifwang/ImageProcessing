2250b38eda6d047f7346849d27ea7a7127776d4a
Zifan <zifwang@mymail.mines.edu>
2019-01-19 18:25:39 -0800

    Modify bilaterFilter.cpp

--- Homework/HW1/src/bilaterFilter.cpp
+++ Homework/HW1/src/bilaterFilter.cpp
@@ -117,11 +117,11 @@ int main(int argc, char *argv[]){
                 for(int fh = -FilterHeight/2; fh <= FilterHeight/2; fh++){
                     for(int fw = -FilterWidth/2; fw <= FilterWidth/2; fw++){
                         double tempF = (pow(fh,2)+pow(fw,2))/(2*pow(sigma_c,2));
-                        double tempS = (pow(abs(double(imageExtend[h][w][channel]) - double(imageExtend[h+fh][w+fw][channel])),2))/double(2*pow(sigma_s,2));
+                        double tempS = (pow(abs(double(imageExtend[h+FilterHeight][w+FilterWidth][channel]) - double(imageExtend[h+FilterHeight+fh][w+FilterWidth+fw][channel])),2))/double(2*pow(sigma_s,2));
                         double weight = exp(-tempF-tempS);
                         // cout << tempF << ", " << tempS << " ";
                         // cout << weight << endl;
-                        num = num + double(imageExtend[h+fh][w+fw][channel])*weight;
+                        num = num + double(imageExtend[h+FilterHeight+fh][w+FilterWidth+fw][channel])*weight;
                         den = den + weight;
                     }
                 }
