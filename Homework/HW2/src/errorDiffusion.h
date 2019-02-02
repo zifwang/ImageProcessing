a7f72e3ea8a00328feab212cc8db3a2179039bef
Zifan <zifwang@mymail.mines.edu>
2019-02-01 19:05:01 -0800

    Enter constant matrix for ErrorDiffusion method

---
+++ Homework/HW2/src/errorDiffusion.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "image.h"
+#include "half_tonning_algorithm.h"
+#include <iostream>
+#include <string>
+
+class errorDiffusion : public half_tonning_algorithm{
+public:
+
+
+
+
+private:
+    // Floyd-Steinberg’s error diffusion with the serpentine scanning
+    const double STEINBERG[3][3] = {{0,0,0},
+                                    {0,0,7/16},
+                                    {3/16,5/16,1/16}};
+
+    // Error diffusion proposed by Jarvis, Judice, and Ninke (JJN)
+    const double JJN[5][5] = {{0,0,0,0,0},
+                              {0,0,0,0,0},
+                              {0,0,0,7/48,5/48},
+                              {3/48,5/48,7/48,5/48,3/48},
+                              {1/48,3/48,5/48,3/48,1/48}};
+
+    // Error diffusion proposed by Stucki
+    const double STUCKI[5][5] = {{0,0,0,0,0},
+                                 {0,0,0,0,0},
+                                 {0,0,0,8/42,4/42},
+                                 {2/42,4/42,8/42,4/42,2/42},
+                                 {1/42,2/42,4/42,2/42,1/42}};
+}
