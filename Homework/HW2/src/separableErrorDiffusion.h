74958ef9526511b1b19130742cb5a252d7759033
Zifan <zifwang@mymail.mines.edu>
2019-02-05 13:27:04 -0800

    Some debug in sed.h

--- Homework/HW2/src/separableErrorDiffusion.h
+++ Homework/HW2/src/separableErrorDiffusion.h
@@ -28,7 +28,9 @@ public:
         double error;
         // apply the Floyd-Steinberg error diffusion algorithm to quantize each channel separately.
         for(int color = 0; color < BytesPerPixel; color++){
+            cout << "Color" << color << endl;
             for(int i = 0; i < imageHeight; i++){
+                cout << "Color " << color << ": " << i << endl;
                 for(int j = 0; j < imageWidth; j++){
                     // compare to the threshold
                     if(tmpImage[i][j][color] >= 127){
@@ -44,8 +46,11 @@ public:
                     if (j-1>=0&&i+1<imageHeight){tmpImage[i+1][j-1][color]=tmpImage[i+1][j-1][color]+error*3/16;}
                     if (i+1<imageHeight){tmpImage[i+1][j][color]=tmpImage[i+1][j][color]+error*5/16;}
                     if (i+1<imageHeight&&j+1<imageWidth){tmpImage[i+1][j+1][color]=tmpImage[i+1][j+1][color]+error*1/16;}
+                    cout << int(imageOut[i][j][color]) << " ";
                 }
+                cout << endl;
             }
+            cout << endl;
                 // else{
                 //     for(int j = imageWidth-1; j >= 0; j--){
                 //         // compare to the threshold
@@ -64,7 +69,20 @@ public:
                 //         if(j < imageWidth-1 && i+1 < imageHeight) tmpImage[color][i+1][j+1] = tmpImage[color][i+1][j+1]+error*3/16;
                 //     }
                 // }
+
         }
+
+        FILE *outFile;
+        outFile = fopen("out_try_sed.raw","wb");
+        if(outFile == NULL){
+            cout << "Error opening file: "<< endl;
+            exit(EXIT_FAILURE);
+        }else{
+            fwrite(imageOut, sizeof(unsigned char), imageHeight*imageWidth*BytesPerPixel, outFile);
+        }
+        fclose(outFile);
+
+
 
         // set outputBuffer
         for(int color = 0; color < BytesPerPixel; color++){
